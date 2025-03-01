aa694ef89dff4b0ef11f2093ae2cc2815f26c4ca
Yurii Rozhak <manrocker1994@gmail.com>
2025-03-01 15:14:16 +0200

    fixed transactions and other minor issues

--- src/main/java/com/example/iotserver/controller/SensorSettingController.java
+++ src/main/java/com/example/iotserver/controller/SensorSettingController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/settings")
@@ -14,14 +15,22 @@ public class SensorSettingController {
 
 
     private final SensorSettingsRepository repository;
+    private final SensorSettingsRepository sensorSettingsRepository;
 
     @PostMapping("/set")
-    public SensorSettings setThreshold(@RequestBody SensorSettings threshold) {
-        return repository.save(threshold);
+    public SensorSettings setSettings(@RequestBody SensorSettings sensorSettings) {
+        SensorSettings existing = repository.findBySensor_Id(sensorSettings.getSensor().getId());
+        return Optional.ofNullable(existing).map(ex -> {
+                    ex.setMaxValue(sensorSettings.getMaxValue());
+                    ex.setMinValue(sensorSettings.getMinValue());
+                    ex.setMaxRecordsStored(sensorSettings.getMaxRecordsStored());
+                    return ex;
+                }).map(sensorSettingsRepository::save)
+                .orElseGet(() -> repository.save(sensorSettings));
     }
 
     @GetMapping("/get/{sensorId}")
-    public SensorSettings getSettings(@PathVariable long sensorId) {
+    public SensorSettings getSettings(@PathVariable(name = "sensorId") long sensorId) {
         return repository.findBySensor_Id(sensorId);
     }
 
--- src/main/java/com/example/iotserver/repository/SensorDataRepository.java
+++ src/main/java/com/example/iotserver/repository/SensorDataRepository.java
@@ -1,6 +1,7 @@
 package com.example.iotserver.repository;
 
 import com.example.iotserver.model.SensorData;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,7 @@ import java.util.List;
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
 
     @Modifying
+    @Transactional
     @Query("DELETE FROM SensorData s WHERE s.id IN " +
             "(SELECT sd.id FROM SensorData sd WHERE sd.sensor.id = :sensorId ORDER BY sd.timestamp ASC LIMIT :deleteCount)")
     void deleteOldestEntries( @Param("deleteCount") long deleteCount, @Param("sensorId") long sensorId);
